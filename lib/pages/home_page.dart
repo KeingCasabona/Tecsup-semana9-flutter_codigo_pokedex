f974cd4c2295f18e5334bb8f20e30886bfe61905
KeingCasabona <keing.limache.casabona@gmail.com>
2024-09-18 15:29:41 -0500

    feat: Add HTTP

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
+  getDataPokemon() async {
+    Uri _uri = Uri.parse(
+        'https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json');
+    http.Response response = await http.get(_uri);
+    print(response.body);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            getDataPokemon();
+          },
+          child: Text('Data'),
+        ),
+      ),
+    );
   }
 }
